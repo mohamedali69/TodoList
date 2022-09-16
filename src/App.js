77655773bc9c1ee1b3d6299dd4559597a70e5764
Mohamed Ali <mohamedalichakhari6901@gmail.com>
2022-09-16 12:57:19 +0100

    todolistapp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
-
+//Importing Components
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Mohamed Ali Todo's List</h1>
       </header>
+      <Form todos={todos} setTodos={setTodos} inputText={inputText} setInputText={setInputText}/>
+      <TodoList setTodos={setTodos} todos={todos}/>
+      
     </div>
   );
 }
-
 export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Todo from './Todo';
+
+const TodoList = ({todos, setTodos}) =>{
+    console.log(todos)
+    return(
+        <div className="todo-container">
+        <ul className="todo-list">
+            {todos.map((todo) => (
+                <Todo setTodos={setTodos} todos={todos} key={todo.id} todo={todo} text={todo.text}/>
+            ))}
+        </ul>
+        
+        </div>
+    )
+}
+
+export default TodoList
